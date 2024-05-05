dc90d51041cc88e5d6ea35b01037519a474a85ef
JuanMeloOli <Juan.Oliveira@sptech.school>
2024-05-05 17:23:57 -0300

    tela de login e cadastro atualizadas

---
+++ cadastrologin.js
@@ -0,0 +1,11 @@
+function mudar(params) {
+  var overlay = document.getElementById("overlay");
+  overlay.style.transform = "translateX(0)";
+  overlay.style.background = "url(assets/zukoArt.jpg)";
+}
+
+function mudar2(params) {
+  var overlay = document.getElementById("overlay");
+  overlay.style.transform = "translateX(100%)";
+  overlay.style.background = "url(assets/kataraArt.jpg)";
+}
