76f519c5f2589f18b981a606327e11f1e07fd84d
JuanMeloOli <Juan.Oliveira@sptech.school>
2024-05-15 23:33:32 -0300

    home atualizada

--- cadastrologin.js
+++ cadastrologin.js
@@ -2,6 +2,7 @@ function mudar(params) {
   var overlay = document.getElementById("overlay");
   overlay.style.transform = "translateX(0)";
   overlay.style.background = "url(assets/zukoArt.jpg)";
+  
 }
 
 function mudar2(params) {
