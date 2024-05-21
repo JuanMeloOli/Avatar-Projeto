38abb7831b0c38af6a12a2ce0c272eb19d166bcf
JuanMeloOli <Juan.Oliveira@sptech.school>
2024-05-20 21:41:16 -0300

    cadastro linkado com banco

--- public/js/cadastrologin.js
+++ public/js/cadastrologin.js
@@ -1,12 +1,88 @@
 function mudar(params) {
   var overlay = document.getElementById("overlay");
   overlay.style.transform = "translateX(0)";
-  overlay.style.background = "url(/public/assets/zukoArt.jpg)";
+  overlay.style.background = "url(./assets/zukoArt.jpg)";
   
 }
 
 function mudar2(params) {
   var overlay = document.getElementById("overlay");
   overlay.style.transform = "translateX(100%)";
-  overlay.style.background = "url(/public/assets/kataraArt.jpg)";
+  overlay.style.background = "url(./assets/kataraArt.jpg)";
+}
+
+
+
+function cadastrar() {
+
+  //Recupere o valor da nova input pelo nome do id
+  // Agora vá para o método fetch logo abaixo
+  var nomeVar = input_name.value;
+  var emailVar = input_email.value;
+  var senhaVar = input_password.value;
+  var confirmacaoSenhaVar = input_confirm.value;
+  var dobraVar = dobraForm.value
+
+  if (
+    nomeVar == "" ||
+    emailVar == "" ||
+    senhaVar == "" ||
+    confirmacaoSenhaVar == "" ||
+    dobraVar == ""
+  ) {
+    return false;
+  }
+
+  // Enviando o valor da nova input
+  fetch("../routes/usuarios.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // crie um atributo que recebe o valor recuperado aqui
+      // Agora vá para o arquivo routes/usuario.js
+      nomeServer: nomeVar,
+      emailServer: emailVar,
+      senhaServer: senhaVar,
+      dobraServer: dobraVar
+    }),
+  })
+    .then(function (resposta) {
+      console.log("resposta: ", resposta);
+
+      if (resposta.ok) {
+        alert("erro")
+
+
+        setTimeout(() => {
+          window.location = "./dashboard/dashboard.js";
+        }, "2000");
+
+
+      } else {
+        throw "Houve um erro ao tentar realizar o cadastro!";
+      }
+    })
+    .catch(function (resposta) {
+      console.log(`#ERRO: ${resposta}`);
+    });
+
+  return false;
+}
+
+function listar() {
+  fetch("/empresas/listar", {
+    method: "GET",
+  })
+    .then(function (resposta) {
+      resposta.json().then((empresas) => {
+        empresas.forEach((empresa) => {
+          listaEmpresas.innerHTML += `<option value='${empresa.id}'>${empresa.cnpj}</option>`;
+        });
+      });
+    })
+    .catch(function (resposta) {
+      console.log(`#ERRO: ${resposta}`);
+    });
 }
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -28,7 +28,6 @@ function autenticar(req, res) {
                   email: resultadoAutenticar[0].email,
                   nome: resultadoAutenticar[0].nome,
                   senha: resultadoAutenticar[0].senha,
-                  aquarios: resultadoAquarios,
                 });
               } else {
                 res.status(204).json({ aquarios: [] });
@@ -56,21 +55,24 @@ function cadastrar(req, res) {
   var nome = req.body.nomeServer;
   var email = req.body.emailServer;
   var senha = req.body.senhaServer;
-  var dobra = req.body.Server;
+  var dobra = req.body.dobraServer;
 
   // Faça as validações dos valores
   if (nome == undefined) {
+    console.log("Seu nome está undefined!")
     res.status(400).send("Seu nome está undefined!");
   } else if (email == undefined) {
+    console.log("Seu email está undefined!")
     res.status(400).send("Seu email está undefined!");
   } else if (senha == undefined) {
+    console.log("Sua senha está undefined!")
     res.status(400).send("Sua senha está undefined!");
   } else if (dobra == undefined) {
+    console.log("Sua Dobra Favorita está undefined!")
     res.status(400).send("Sua Dobra Favorita está undefined!");
   } else {
     // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    usuarioModel
-      .cadastrar(nome, email, senha, dobra)
+    usuarioModel.cadastrar(nome, email, senha, dobra)
       .then(function (resultado) {
         res.json(resultado);
       })
