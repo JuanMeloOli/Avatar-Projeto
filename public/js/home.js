17eb179118bc4d5b247155dc58070a743ae1741a
JuanMeloOli <Juan.Oliveira@sptech.school>
2024-05-22 22:08:59 -0300

    teste no note

--- public/js/home.js
+++ public/js/home.js
@@ -12,3 +12,21 @@ var show = document.querySelector(".trailer");
 setTimeout(function () {
   show.classList.add("show");
 }, 1000);
+
+const cardLogin = document.getElementById("cardLogin");
+
+function close(params) {
+  cardLogin.style.display = "none";
+}
+
+function goQuiz(params) {
+  if (sessionStorage.NOME_USUARIO == undefined) {
+    cardLogin.style.display = "flex";
+  } else {
+    window.location = "./water-quiz.html";
+  }
+}
+
+function goAgniKai(params) {}
+
+function goTophGame(params) {}
